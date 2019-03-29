12aedefce023afe5741fe559d59aca2d85d538fa
Neyrin <isa.areschoug@gmail.com>
2019-03-29 10:58:37 +0100

    Added get, set for OrderInfo model

--- WebShop/Models/OrderInfo.cs
+++ WebShop/Models/OrderInfo.cs
@@ -14,5 +14,15 @@ namespace WebShop.Models
 {
     public class OrderInfo
     {
+        public int OrderId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phonenumber { get; set; }
+        public string Email { get; set; }
+        public string SteetAddress { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public float TotalPrice { get; set; }
+        public string OrderDate { get; set; }
     }
 }
