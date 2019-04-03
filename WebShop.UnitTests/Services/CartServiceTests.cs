e155942d6673664e464db5ba291786a638f138ab
Neyrin <isa.areschoug@gmail.com>
2019-04-03 16:32:11 +0200

    Unit test- WIP

---
+++ WebShop.UnitTests/Services/CartServiceTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using FakeItEasy;
+using NUnit.Framework;
+using WebShop.Repositories;
+using WebShop.Services;
+using WebShop.Models;
+using System.Linq;
+
+namespace WebShop.UnitTests.Services
+{
+    class CartServiceTests
+    {
+        private ICartRepository cartRepository;
+        private CartService newsService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.cartRepository = A.Fake<ICartRepository>();
+            this.newsService = new CartService(this.cartRepository);
+        }
+
+        [Test]
+        public void Get_ReturnsResultFromRepository()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Test]
+        public void Get_GivenId_ReturnsResultFromRepository()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Test]
+        public void Add_GivenValidIds_CallsCartRepository_ThenReturnsTrue()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
+        //[Test]
+        //public void //Test delete here but how the fuck do I do that?()    
+        //{
+            //Arrange
+            //Act
+            //Assert
+        //}
+    }
+}
+
+
---
+++ WebShop/Repositories/ICartRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebShop.Models;
+
+namespace WebShop.Repositories
+{
+    public class ICartRepository
+    {
+        List<Cart> Get();
+        Cart Get(int cartId);
+        void Add(Cart cart);
+        void Delete(int cartId);
+    }
+}
