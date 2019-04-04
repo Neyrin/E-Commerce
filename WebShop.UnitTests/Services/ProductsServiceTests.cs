32bfe9736252e738a327da00762262d452694370
Neyrin <isa.areschoug@gmail.com>
2019-04-04 22:51:34 +0200

    Backup after reset

---
+++ WebShop.UnitTests/Services/ProductsServiceTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using FakeItEasy;
+using NUnit.Framework;
+using WebShop.Repositories;
+using WebShop.Services;
+using WebShop.Models;
+using System.Linq;
+
+namespace WebShop.UnitTests.Services
+{
+    class ProductsServiceTests
+    {
+        private ProductsService productsService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.productsService = new ProductsService(
+             new ProductsRepository("Data Source=(LocalDB)\\MSSQLLocalDB;Initial Catalog=EC1cmtq300;Integrated Security=True;Pooling=True"));
+        }
+
+        [Test]
+        public void Get_returnsItemsFromDatabase()
+        {
+            //act
+            var results = this.productsService.Get();
+            //assert
+            Assert.That(results.Count, Is.EqualTo(2));
+
+            Assert.That(results[0].ProductId, Is.EqualTo(2));
+            Assert.That(results[0].ProductName, Is.EqualTo("Highway tunnel"));
+            Assert.That(results[0].ProductType, Is.EqualTo("Poster"));
+            Assert.That(results[0].Price, Is.EqualTo(12));
+        }
+
+        //[Test]
+        //public void Get_GivenId_ReturnsResultFromRepository()
+        //{
+        //    // Arrange
+        //    // Act
+        //    // Assert
+        //}
+
+        //[Test]
+        //public void Add_GivenValidIds_CallsProductsRepository_ThenReturnsTrue()
+        //{
+        //    //Arrange
+        //    //Act
+        //    //Assert
+        //}
+    }
+}
+
+
